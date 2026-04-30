19b36176ce588fba895f0aecce5f5865788e73d6
Jeovânio Reis <jeovanio99.jheo@gmail.com>
2026-04-29 21:23:26 -0300

    Create Produto.java
    feat: criação da classe Produto

---
+++ src/Produto.java
@@ -0,0 +1,13 @@
+class Produto {
+    String nome;
+    double preco;
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public void exibirDados() {
+        System.out.println(nome + " - R$ " + preco);
+    }
+}
