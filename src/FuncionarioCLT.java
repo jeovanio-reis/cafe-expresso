854787946c82813dc021a09dfd05a0ea7c5cf810
jeizaalveschagas-lab <jeizaalveschagas@gmail.com>
2026-04-29 22:03:24 -0300

    Update FuncionarioCLT.java

--- src/FuncionarioCLT.java
+++ src/FuncionarioCLT.java
@@ -1,1 +1,13 @@
+class FuncionarioCLT extends Funcionario {
 
+    double salarioBase;
+
+    public FuncionarioCLT(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+    @Override
+    public double calcularSalario() {
+        return salarioBase;
+    }
+}
