3b5177c432762da945c13d84f0b51c3de1c47632
jeizaalveschagas-lab <jeizaalveschagas@gmail.com>
2026-04-29 22:08:07 -0300

    Update FuncionarioFreelancer.java

--- src/FuncionarioFreelancer.java
+++ src/FuncionarioFreelancer.java
@@ -1,1 +1,15 @@
+class FuncionarioFreelancer extends Funcionario {
 
+    int horasTrabalhadas;
+    double valorHora;
+
+    public FuncionarioFreelancer(int horasTrabalhadas, double valorHora) {
+        this.horasTrabalhadas = horasTrabalhadas;
+        this.valorHora = valorHora;
+    }
+
+    @Override
+    public double calcularSalario() {
+        return horasTrabalhadas * valorHora;
+    }
+}
