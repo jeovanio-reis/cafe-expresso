2f16d116fb9a8b89a84561301b6c235b7872a8f8
jeizaalveschagas-lab <jeizaalveschagas@gmail.com>
2026-05-03 14:07:11 -0300

    Criação da classe produto

--- src/main/java/ItemPedido.java
+++ src/main/java/ItemPedido.java
@@ -1,1 +1,14 @@
+public class ItemPedido {
 
+    private Produto produto;
+    private int quantidade;
+
+    public ItemPedido(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public double calcularSubtotal() {
+        return produto.getPreco() * quantidade;
+    }
+}
