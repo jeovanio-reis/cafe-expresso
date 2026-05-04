7f0b325ef2713d52713d91b21cdf2085b4cd6be1
Jeovânio Reis <jeovanio99.jheo@gmail.com>
2026-05-04 20:23:18 -0300

    Update Pedido.java
    feat: criar classe pedido com lista de itens

--- src/main/java/Pedido.java
+++ src/main/java/Pedido.java
@@ -1,1 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
 
+public class Pedido {
+
+    private List<ItemPedido> itens;
+    private String status;
+
+    public Pedido() {
+        this.itens = new ArrayList<>();
+        this.status = "Pendente";
+    }
+
+    public void adicionarItem(ItemPedido item) {
+        itens.add(item);
+    }
+
+    public double calcularTotal() {
+        double total = 0;
+        for (ItemPedido item : itens) {
+            total += item.calcularSubtotal();
+        }
+        return total;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<ItemPedido> getItens() {
+        return itens;
+    }
+}
